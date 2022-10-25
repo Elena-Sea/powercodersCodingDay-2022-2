4cef2d7fab152a73705c0fa01af650b334e56004
Elena-Sea <o.smetyuk@gmail.com>
2022-10-25 15:59:16 +0200

    adds task 6

---
+++ 06_exercise/index.js
@@ -0,0 +1,8 @@
+const weekDays = ["Monday", "Thursday", "Saturday", "Wednesday", "Tuesday"];
+
+const input = document.querySelector('.input');
+input.innerHTML = weekDays;
+const output = document.querySelector('.output');
+
+output.innerHTML = weekDays.sort();
+
