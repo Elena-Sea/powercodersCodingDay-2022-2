217e160f8ed40aa64650964a25b5a69c2c392875
Elena-Sea <o.smetyuk@gmail.com>
2022-10-25 16:03:31 +0200

    adds func last in array in task6

--- 06_exercise/index.js
+++ 06_exercise/index.js
@@ -3,6 +3,11 @@ const weekDays = ["Monday", "Thursday", "Saturday", "Wednesday", "Tuesday"];
 const input = document.querySelector('.input');
 input.innerHTML = weekDays;
 const output = document.querySelector('.output');
+const outputLast = document.querySelector('.output-last');
+let sortedWeek = weekDays.sort();
+output.innerHTML = sortedWeek;
+function getLastValue() { 
+    outputLast.innerHTML = sortedWeek[sortedWeek.length - 1];
+}
 
-output.innerHTML = weekDays.sort();
-
+getLastValue()
